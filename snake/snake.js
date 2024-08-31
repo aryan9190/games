41722d5ec55156e8c18b8e7ed6f431f4e3ad128e
aryan9190 <115806023+aryan9190@users.noreply.github.com>
2024-08-31 20:08:16 +0530

    Create snake.js

---
+++ snake/snake.js
@@ -0,0 +1,59 @@
+const canvas = document.getElementById("snakeCanvas");
+const ctx = canvas.getContext("2d");
+
+const box = 20;
+let snake = [{ x: 10 * box, y: 10 * box }];
+let food = { x: Math.floor(Math.random() * 20) * box, y: Math.floor(Math.random() * 20) * box };
+let score = 0;
+let direction = "";
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowUp" && direction !== "DOWN") direction = "UP";
+    else if (event.key === "ArrowDown" && direction !== "UP") direction = "DOWN";
+    else if (event.key === "ArrowLeft" && direction !== "RIGHT") direction = "LEFT";
+    else if (event.key === "ArrowRight" && direction !== "LEFT") direction = "RIGHT";
+});
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = i === 0 ? "green" : "lightgreen";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+
+    ctx.fillStyle = "red";
+    ctx.fillRect(food.x, food.y, box, box);
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction === "UP") snakeY -= box;
+    else if (direction === "DOWN") snakeY += box;
+    else if (direction === "LEFT") snakeX -= box;
+    else if (direction === "RIGHT") snakeX += box;
+
+    if (snakeX === food.x && snakeY === food.y) {
+        score++;
+        food = { x: Math.floor(Math.random() * 20) * box, y: Math.floor(Math.random() * 20) * box };
+    } else {
+        snake.pop();
+    }
+
+    const newHead = { x: snakeX, y: snakeY };
+
+    if (snakeX < 0 || snakeX >= canvas.width || snakeY < 0 || snakeY >= canvas.height || collision(newHead, snake)) {
+        clearInterval(game);
+    }
+
+    snake.unshift(newHead);
+}
+
+function collision(head, snakeArray) {
+    for (let i = 0; i < snakeArray.length; i++) {
+        if (head.x === snakeArray[i].x && head.y === snakeArray[i].y) return true;
+    }
+    return false;
+}
+
+const game = setInterval(draw, 100);
